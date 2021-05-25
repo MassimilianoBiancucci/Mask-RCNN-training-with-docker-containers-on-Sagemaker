0a5867d3c16a77fa8f204b432e6dbb26942186ff
simone-viozzi <viozzis333@gmail.com>
2021-05-25 22:41:30 +0200

    - riformattato il documento Mask_RCNN/lesions_sagemaker.py e ristrutturato un po' (funziona) - aggiunte delle cartelle al .gitignore

--- Mask_RCNN/lesions_sagemaker.py
+++ Mask_RCNN/lesions_sagemaker.py
@@ -2,8 +2,19 @@
 Script for Mask_R-CNN training 
 '''
 
+import cv2
+import random
+import imutils
+import argparse
+import numpy as np
+from imutils import paths
+from mrcnn import utils
+from mrcnn import visualize
+from mrcnn import model as modellib
+from mrcnn.config import Config
+from imgaug import augmenters as iaa
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '4' 
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '4'
 
 '''
 TF DEBUG LEVELS:
@@ -14,27 +25,16 @@ TF DEBUG LEVELS:
 '''
 
 # import the necessary packages
-from imgaug import augmenters as iaa
-from mrcnn.config import Config
-from mrcnn import model as modellib
-from mrcnn import visualize
-from mrcnn import utils
-from imutils import paths
-import numpy as np
-import argparse
-import imutils
-import random
-import cv2
 
 
 # initialize the dataset path, images path, and annotations file path
-#ATTENZONE MODIFICARE CON LA POSIZIONE CORRETTA SE NECESSARIO
+# ATTENZONE MODIFICARE CON LA POSIZIONE CORRETTA SE NECESSARIO
 DATASET_PATH = os.path.abspath("/root/isic2018")
 #DATASET_PATH = os.path.abspath("isic2018")
 IMAGES_PATH = os.path.sep.join([DATASET_PATH,
-	"ISIC2018_Task1-2_Training_Input"])
+                                "ISIC2018_Task1-2_Training_Input"])
 MASKS_PATH = os.path.sep.join([DATASET_PATH,
-	"ISIC2018_Task1_Training_GroundTruth"])
+                               "ISIC2018_Task1_Training_GroundTruth"])
 
 # initialize the amount of data to use for training
 TRAINING_SPLIT = 0.8
@@ -66,182 +66,203 @@ COCO_PATH = "mask_rcnn_coco.h5"
 # snapshots will be stored
 LOGS_AND_MODEL_DIR = "lesions_logs"
 
+
 class LesionBoundaryConfig(Config):
-	"""
-	estendo la classe Config di maskrcnn (mrcnn/config.py) che contiene le 
-	configurazini di default e ovverrido quelle che voglio modificare.
-	"""	
+    """
+    estendo la classe Config di maskrcnn (mrcnn/config.py) che contiene le 
+    configurazini di default e ovverrido quelle che voglio modificare.
+    """
 
-	# give the configuration a recognizable name
-	NAME = "lesion"
+    # give the configuration a recognizable name
+    NAME = "lesion"
 
-	# set the number of GPUs to use training along with the number of
-	# images per GPU (which may have to be tuned depending on how
-	# much memory your GPU has)
-	GPU_COUNT = 1
-	IMAGES_PER_GPU = 1
+    # set the number of GPUs to use training along with the number of
+    # images per GPU (which may have to be tuned depending on how
+    # much memory your GPU has)
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
 
-	# set the number of steps per training epoch and validation cycle
-	STEPS_PER_EPOCH = len(trainIdxs) // (IMAGES_PER_GPU * GPU_COUNT)
-	VALIDATION_STEPS = len(valIdxs) // (IMAGES_PER_GPU * GPU_COUNT)
+    # set the number of steps per training epoch and validation cycle
+    #STEPS_PER_EPOCH = len(trainIdxs) // (IMAGES_PER_GPU * GPU_COUNT)
+    #VALIDATION_STEPS = len(valIdxs) // (IMAGES_PER_GPU * GPU_COUNT)
 
-	# number of classes (+1 for the background)
-	NUM_CLASSES = len(CLASS_NAMES) + 1
+    # number of classes (+1 for the background)
+    #NUM_CLASSES = len(CLASS_NAMES) + 1
+
+    def __init__(self, STEPS_PER_EPOCH,  VALIDATION_STEPS, NUM_CLASSES):
+        self.STEPS_PER_EPOCH = STEPS_PER_EPOCH
+        self.VALIDATION_STEPS = VALIDATION_STEPS
+        self.NUM_CLASSES = NUM_CLASSES
+        super().__init__(self)
 
 
 class LesionBoundaryInferenceConfig(LesionBoundaryConfig):
-	# set the number of GPUs and images per GPU (which may be
-	# different values than the ones used for training)
-	GPU_COUNT = 1
-	IMAGES_PER_GPU = 1
+    # set the number of GPUs and images per GPU (which may be
+    # different values than the ones used for training)
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
 
-	# set the minimum detection confidence (used to prune out false
-	# positive detections)
-	DETECTION_MIN_CONFIDENCE = 0.9
+    # set the minimum detection confidence (used to prune out false
+    # positive detections)
+    DETECTION_MIN_CONFIDENCE = 0.9
 
 
 class LesionBoundaryDataset(utils.Dataset):
-	def __init__(self, imagePaths, classNames, width=1024):
-		# call the parent constructor
-		super().__init__(self)
+    def __init__(self, imagePaths, classNames, width=1024):
+        # call the parent constructor
+        super().__init__(self)
 
-		# store the image paths and class names along with the width
-		# we'll resize images to
-		self.imagePaths = imagePaths
-		self.classNames = classNames
-		self.width = width
+        # store the image paths and class names along with the width
+        # we'll resize images to
+        self.imagePaths = imagePaths
+        self.classNames = classNames
+        self.width = width
 
-	def load_lesions(self, idxs):
-		"""load the dataset from the disk into the dataset class
+    def load_lesions(self, idxs):
+        """load the dataset from the disk into the dataset class
 
-		Args:
-			idxs (list of int): gli indici che determinano l'ordine 
-						delle immagini nel dataset
-		"""		
+        Args:
+                idxs (list of int): gli indici che determinano l'ordine 
+                                        delle immagini nel dataset
+        """
 
-		# loop over all class names and add each to the 'lesion'
-		# dataset
-		for (classID, label) in self.classNames.items():
-			self.add_class("lesion", classID, label)
+        # loop over all class names and add each to the 'lesion'
+        # dataset
+        for (classID, label) in self.classNames.items():
+            self.add_class("lesion", classID, label)
 
-		# loop over the image path indexes
-		for i in idxs:
-			# extract the image filename to serve as the unique
-			# image ID
-			imagePath = self.imagePaths[i]
-			filename = imagePath.split(os.path.sep)[-1]
+        # loop over the image path indexes
+        for i in idxs:
+            # extract the image filename to serve as the unique
+            # image ID
+            imagePath = self.imagePaths[i]
+            filename = imagePath.split(os.path.sep)[-1]
 
-			# add the image to the dataset
-			self.add_image("lesion", image_id=filename, path=imagePath)
+            # add the image to the dataset
+            self.add_image("lesion", image_id=filename, path=imagePath)
 
-	#override
-	def load_image(self, imageID):
-		# grab the image path, load it, and convert it from BGR to
-		# RGB color channel ordering
-		p = self.image_info[imageID]["path"]
-		image = cv2.imread(p)
-		image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    # override
+    def load_image(self, imageID):
+        # grab the image path, load it, and convert it from BGR to
+        # RGB color channel ordering
+        p = self.image_info[imageID]["path"]
+        image = cv2.imread(p)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-		# resize the image, preserving the aspect ratio
-		image = imutils.resize(image, width=self.width)
+        # resize the image, preserving the aspect ratio
+        image = imutils.resize(image, width=self.width)
 
-		# return the image
-		return image
+        # return the image
+        return image
 
-	#override
-	def load_mask(self, imageID):
-		# grab the image info and derive the full annotation path
-		# file path
-		info = self.image_info[imageID]
-		filename = info["id"].split(".")[0]
-		annotPath = os.path.sep.join([MASKS_PATH,
-			"{}_segmentation.png".format(filename)])
+    # override
+    def load_mask(self, imageID):
+        # grab the image info and derive the full annotation path
+        # file path
+        info = self.image_info[imageID]
+        filename = info["id"].split(".")[0]
+        annotPath = os.path.sep.join([MASKS_PATH,
+                                      "{}_segmentation.png".format(filename)])
 
-		# load the annotation mask and resize it, *making sure* to
-		# use nearest neighbor interpolation
-		annotMask = cv2.imread(annotPath)
-		annotMask = cv2.split(annotMask)[0]
-		annotMask = imutils.resize(annotMask, width=self.width,
-			inter=cv2.INTER_NEAREST)
-		annotMask[annotMask > 0] = 1
+        # load the annotation mask and resize it, *making sure* to
+        # use nearest neighbor interpolation
+        annotMask = cv2.imread(annotPath)
+        annotMask = cv2.split(annotMask)[0]
+        annotMask = imutils.resize(annotMask, width=self.width,
+                                   inter=cv2.INTER_NEAREST)
+        annotMask[annotMask > 0] = 1
 
-		# determine the number of unique class labels in the mask
-		classIDs = np.unique(annotMask)
+        # determine the number of unique class labels in the mask
+        classIDs = np.unique(annotMask)
 
-		# the class ID with value '0' is actually the background
-		# which we should ignore and remove from the unique set of
-		# class identifiers
-		classIDs = np.delete(classIDs, [0])
+        # the class ID with value '0' is actually the background
+        # which we should ignore and remove from the unique set of
+        # class identifiers
+        classIDs = np.delete(classIDs, [0])
 
-		# allocate memory for our [height, width, num_instances]
-		# array where each "instance" effectively has its own
-		# "channel" -- since there is only one lesion per image we
-		# know the number of instances is equal to 1
-		masks = np.zeros((annotMask.shape[0], annotMask.shape[1], 1),
-			dtype="uint8")
+        # allocate memory for our [height, width, num_instances]
+        # array where each "instance" effectively has its own
+        # "channel" -- since there is only one lesion per image we
+        # know the number of instances is equal to 1
+        masks = np.zeros((annotMask.shape[0], annotMask.shape[1], 1),
+                         dtype="uint8")
 
-		# loop over the class IDs
-		for (i, classID) in enumerate(classIDs):
-			# construct a mask for *only* the current label
-			classMask = np.zeros(annotMask.shape, dtype="uint8")
-			classMask[annotMask == classID] = 1
+        # loop over the class IDs
+        for (i, classID) in enumerate(classIDs):
+            # construct a mask for *only* the current label
+            classMask = np.zeros(annotMask.shape, dtype="uint8")
+            classMask[annotMask == classID] = 1
 
-			# store the class mask in the masks array
-			masks[:, :, i] = classMask
+            # store the class mask in the masks array
+            masks[:, :, i] = classMask
 
-		# return the mask array and class IDs
-		return (masks.astype("bool"), classIDs.astype("int32"))
+        # return the mask array and class IDs
+        return (masks.astype("bool"), classIDs.astype("int32"))
+
 
 if __name__ == "__main__":
-	# construct the argument parser and parse the arguments
-	ap = argparse.ArgumentParser()
+    # construct the argument parser and parse the arguments
+    ap = argparse.ArgumentParser()
 
-	# il dataset non puo' essere passato con gli arg poiche' le classi config
-	# devono sapere come e' fatto il dataset all'avvio del programma
-	# parser.add_argument("--dataset", type=str, default=os.environ["SM_CHANNEL_DATASET"])
-	
-	args = vars(ap.parse_args())
+    # il dataset non puo' essere passato con gli arg poiche' le classi config
+    # devono sapere come e' fatto il dataset all'avvio del programma
+    # parser.add_argument("--dataset", type=str, default=os.environ["SM_CHANNEL_DATASET"])
 
-	# load the training dataset
-	trainDataset = LesionBoundaryDataset(IMAGE_PATHS, CLASS_NAMES)
-	trainDataset.load_lesions(trainIdxs)
-	trainDataset.prepare()
+    args = vars(ap.parse_args())
 
-	# load the validation dataset
-	valDataset = LesionBoundaryDataset(IMAGE_PATHS, CLASS_NAMES)
-	valDataset.load_lesions(valIdxs)
-	valDataset.prepare()
+    # load the training dataset
+    trainDataset = LesionBoundaryDataset(IMAGE_PATHS, CLASS_NAMES)
+    trainDataset.load_lesions(trainIdxs)
+    trainDataset.prepare()
 
-	# initialize the training configuration
-	config = LesionBoundaryConfig()
-	config.display()
+    # load the validation dataset
+    valDataset = LesionBoundaryDataset(IMAGE_PATHS, CLASS_NAMES)
+    valDataset.load_lesions(valIdxs)
+    valDataset.prepare()
 
-	# initialize the image augmentation process
-	# fa l'argomentazione con al massimo 2 tipi di argomentazione
-	aug = iaa.SomeOf((0, 2), [
-		iaa.Fliplr(0.5),
-		iaa.Flipud(0.5),
-		iaa.Affine(rotate=(-10, 10))
-	])
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
 
-	# initialize the model and load the COCO weights so we can
-	# perform fine-tuning
-	model = modellib.MaskRCNN(mode="training", config=config,
-		model_dir=LOGS_AND_MODEL_DIR) # separare log e model
+    # initialize the training configuration
+    # set the number of steps per training epoch and validation cycle
+    STEPS_PER_EPOCH = len(trainIdxs) // (IMAGES_PER_GPU * GPU_COUNT)
+    VALIDATION_STEPS = len(valIdxs) // (IMAGES_PER_GPU * GPU_COUNT)
 
-	model.load_weights(COCO_PATH, by_name=True,
-		exclude=["mrcnn_class_logits", "mrcnn_bbox_fc",
-			"mrcnn_bbox", "mrcnn_mask"])
+    # number of classes (+1 for the background)
+    NUM_CLASSES = len(CLASS_NAMES) + 1
 
-	# train *just* the layer heads
-	model.train(trainDataset, valDataset, epochs=2,
-		layers="heads", learning_rate=config.LEARNING_RATE,
-		augmentation=aug)
+    config = LesionBoundaryConfig(
+        STEPS_PER_EPOCH, VALIDATION_STEPS, NUM_CLASSES)
+    config.display()
 
-	# unfreeze the body of the network and train *all* layers
-	model.train(trainDataset, valDataset, epochs=2,
-		layers="all", learning_rate=config.LEARNING_RATE / 10,
-		augmentation=aug)
+    # initialize the image augmentation process
+    # fa l'argomentazione con al massimo 2 tipi di argomentazione
+    aug = iaa.SomeOf((0, 2), [
+        iaa.Fliplr(0.5),
+        iaa.Flipud(0.5),
+        iaa.Affine(rotate=(-10, 10))
+    ])
+
+    # initialize the model and load the COCO weights so we can
+    # perform fine-tuning
+    model = modellib.MaskRCNN(mode="training", config=config,
+                              model_dir=LOGS_AND_MODEL_DIR)  # separare log e model
+
+    model.load_weights(COCO_PATH, by_name=True,
+                       exclude=["mrcnn_class_logits", "mrcnn_bbox_fc",
+                                "mrcnn_bbox", "mrcnn_mask"])
+
+    # train *just* the layer heads
+    model.train(trainDataset, valDataset, epochs=2,
+                layers="heads", learning_rate=config.LEARNING_RATE,
+                augmentation=aug)
+
+    # unfreeze the body of the network and train *all* layers
+    model.train(trainDataset, valDataset, epochs=1,
+                layers="all", learning_rate=config.LEARNING_RATE / 10,
+                augmentation=aug)
+
+    print("goodbye!")
 
 '''
 sample output:
